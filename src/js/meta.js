491a26287ef4e708f2d00ec8e7868fa7e7a5f54e
Bergkamp Ho <hello@bergkampho.com>
2024-02-27 15:34:10 +0000

    Reuse metadata

---
+++ src/js/meta.js
@@ -0,0 +1,8 @@
+const meta = {
+  title: "Hello! I'm Wayne.",
+  subtitle: "Bergkamp Ho is my network ID.",
+  description:
+    "I'm a Full Stack Web Developer with 14+ years of experience in fintech, software engineering and marketing industry.",
+};
+
+export default meta;
