4cf4d5b8fcf65ec69a27ae2c4c87bc9047901b98
Bergkamp Ho <hello@bergkampho.com>
2024-10-25 17:24:47 +0100

    Refine metadata

--- src/js/meta.js
+++ src/js/meta.js
@@ -2,7 +2,7 @@ const meta = {
   title: "Hello! I'm Wayne.",
   subtitle: "Bergkamp Ho is my network ID.",
   description:
-    "I'm a web developer passionate about crafting code that address real-life challenges.",
+    "I'm a web developer who loves to write code that solves real-life problems.",
 };
 
 export default meta;
